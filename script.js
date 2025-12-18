80074f4b5eae4ba0ee29eb211c5468eeeb680935
nadaajalah <nabilatunnadaa2@gmail.com>
2025-12-18 12:14:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,42 @@ function login() {
   const user = document.getElementById("username").value;
   const pass = document.getElementById("password").value;
 
-  if(user && pass) {
-    localStorage.setItem("login", "true");
-    window.location.href = "dashboard.html";
+  if (user && pass) {
+    document.getElementById("loginPage").style.display = "none";
+    document.getElementById("dashboardPage").style.display = "block";
   } else {
-    alert("Isi username dan password!");
+    alert("Username dan password wajib diisi!");
   }
 }
 
 function logout() {
-  localStorage.removeItem("login");
-  window.location.href = "index.html";
+  document.getElementById("dashboardPage").style.display = "none";
+  document.getElementById("loginPage").style.display = "flex";
 }
 
-if (window.location.pathname.includes("dashboard")) {
-  if (!localStorage.getItem("login")) {
-    window.location.href = "index.html";
+function klasifikasi() {
+  let skor = 0;
+
+  if (document.getElementById("plastik").checked) skor -= 2;
+  if (document.getElementById("ramah").checked) skor += 2;
+  if (document.getElementById("bekal").checked) skor += 3;
+  if (document.getElementById("buang").checked) skor -= 3;
+
+  let kategori = "";
+  let hasil = "";
+
+  if (skor >= 3) {
+    hasil = "Ramah Lingkungan";
+    kategori = "Baik";
+  } else if (skor >= 0) {
+    hasil = "Cukup Ramah Lingkungan";
+    kategori = "Perlu Perbaikan";
+  } else {
+    hasil = "Tidak Ramah Lingkungan";
+    kategori = "Tidak Ramah Lingkungan";
   }
+
+  document.getElementById("skor").innerText = hasil;
+  document.getElementById("kategori").innerText = kategori;
+  document.getElementById("hasil").style.display = "block";
 }
